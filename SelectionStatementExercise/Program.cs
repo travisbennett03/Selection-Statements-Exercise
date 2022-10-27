012bec7874241af1a942a62e03e055f187c0ff67
travisbennett03 <tbennett1996@yahoo.com>
2022-10-27 13:10:52 -0500

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+            Console.WriteLine($"This is what the number was {favNumber}");
         }
     }
 }
